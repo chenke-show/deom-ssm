4655c037af3bece75afeaec08fe359e9f163e560
chenke-show <65203419+chenke-show@users.noreply.github.com>
2020-05-14 11:36:29 +0800

    T.java

--- src/main/java/com/jbit/test/T.java
+++ src/main/java/com/jbit/test/T.java
@@ -3,6 +3,7 @@ package com.jbit.test;
 public class T {
     public static void main(String[] args) {
         System.out.print("hello Word!");
+        //测试
         System.out.print("hello Word!");
     }
 }
