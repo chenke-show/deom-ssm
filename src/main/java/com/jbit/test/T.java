c3ff60a68b96a99946effac67210be2a5324a3f9
chenke <1476466400@qq.com>
2020-05-14 11:14:37 +0800

    Initial commit

---
+++ src/main/java/com/jbit/test/T.java
@@ -0,0 +1,8 @@
+package com.jbit.test;
+
+public class T {
+    public static void main(String[] args) {
+        System.out.print("hello Word!");
+        System.out.print("hello Word!");
+    }
+}
